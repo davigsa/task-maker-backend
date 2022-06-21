f6c7e8ccabeabd999396e2fb25c1519a0657346f
Davi Sá <davi.goulartsa@gmail.com>
2022-06-21 07:22:41 -0300

    Adding other routes to project

--- src/app/repositories/ProjectRepository.js
+++ src/app/repositories/ProjectRepository.js
@@ -2,13 +2,40 @@ const Project = require('../models/Project')
 const HttpException = require('../../common/http-exception')
 
 const projectRepository = {
-    create: create
+	create,
+	findByUserId,
+	deleteById,
+	updateById
 }
 
-async function create ({ projectName, userId }) {
+async function create({ projectName, userId }) {
 	const newProject = new Project({ projectName, userId })
-	
+
 	return newProject.save()
+}
+
+async function findByUserId({ userId }) {
+	const projects = await Project.findAll({ where: { userId }})
+
+	return projects
+}
+
+async function deleteById({ id, userId }) {
+	const project = await Project.findOne({ where: { id }})
+
+	if (!project) throw new HttpException(404, 'Project not found')
+	if (project.userId !== userId) throw new HttpException(401, 'Unauthorized')
+
+	await Project.destroy({ where: { id }})
+}
+
+async function updateById({ id, projectName, userId }) {
+	const project = await Project.findOne({ where: { id }})
+
+	if (!project) throw new HttpException(404, 'Project not found')
+	if (project.userId !== userId) throw new HttpException(401, 'Unauthorized')
+
+	await Project.update({ projectName }, { where: { id }})
 }
 
 module.exports = projectRepository
--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -11,7 +11,46 @@ projectRoutes.post('/', authMiddleware, async (req, res, next) => {
 
 	try {
 		const project = await projectRepository.create({ projectName, userId })
-		
+
+    return res.status(201).send(project)
+  } catch (e) {
+    res.status(e.statusCode).send(e.message)
+  }
+})
+
+projectRoutes.get('/', authMiddleware, async (req, res, next) => {
+	const userId = req.userId
+
+	try {
+		const projects = await projectRepository.findByUserId({ userId })
+
+    return res.status(200).send(projects)
+  } catch (e) {
+    res.status(e.statusCode).send(e.message)
+  }
+})
+
+projectRoutes.delete('/:id', authMiddleware, async (req, res, next) => {
+	const userId = req.userId
+	const id = req.params.id 
+
+	try {
+		await projectRepository.deleteById({ id, userId })
+
+    return res.status(204)
+  } catch (e) {
+    res.status(e.statusCode).send(e.message)
+  }
+})
+
+projectRoutes.put('/:id', authMiddleware, async (req, res, next) => {
+	const userId = req.userId
+	const id = req.params.id
+	const { projectName } = req.body
+
+	try {
+		const project = await projectRepository.updateById({ id, projectName, userId })
+
     return res.status(201).send(project)
   } catch (e) {
     res.status(e.statusCode).send(e.message)
